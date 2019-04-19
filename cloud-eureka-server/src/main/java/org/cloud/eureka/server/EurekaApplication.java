6cf2627ec955c0d2503269396568ad26daa5b942
LiuGangQiang <LiuGangQiang@gongyong-PC>
2019-04-19 16:23:02 +0800

    优化日志

--- cloud-eureka-server/src/main/java/org/cloud/eureka/server/EurekaApplication.java
+++ cloud-eureka-server/src/main/java/org/cloud/eureka/server/EurekaApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Copyright © 2018 Fist Team. All rights reserved.
+ * Copyright © 2019 ChengDu Smart Technology Co.Ltd All Rights Reserved.
  *
- * @author: LiuGangQiang
- * @date: 2018年12月10日
- * @description: 注册中心主函数类
+ * @since 2019/04/19
+ * @author LiuGangQiang
+ * @project cloud-eureka-server
+ * @package org.cloud.eureka.server
+ * @remark register main
  */
 @SpringBootApplication
 @EnableEurekaServer
